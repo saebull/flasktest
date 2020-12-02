c64ccee45dd142840adfceca80e6177716bbd7ed
saebull <73066922+saebull@users.noreply.github.com>
2020-12-02 23:42:17 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('ongkey.json')
+defaul_app = firebase_admin.initialize_app(cred,{
+    'databaseURL' : 'https://ie-project-292614.firebaseio.com/'
+})
+ref = db.reference('/road')
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
